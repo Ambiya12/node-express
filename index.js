00870a2db8348a0a3e3877993cca342e89e26de6
Ambiya12 <ambiyadms@gmail.com>
2025-03-04 15:28:04 +0100

    feat: refactor API routes to use router and environment variables

--- index.js
+++ index.js
@@ -1,53 +1,22 @@
 import express from 'express';
 //Import express from the express module
-import games from './data/games.js';
+import gamesRouter from './routes/gamesRouter.js';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 // App take everything from express
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', gamesRouter);
 
 app.get('/', (request, response) => {
-    return response.end('Welcome to my games API');
-});
-// Our first route that takes the path as first parameters, request and response and returns a response
-
-app.get('/games', (request, response) => {
-    return response.json(games);
-});
-// Route to get all games
-
-app.get('/games/:id', (request, response) => {
-    const gamesID = request.params.id;
-    // I save onto a variable the value of the params
-    const gamesByID = games.find(game => game.id == gamesID);
-    // I search in the array where i do have an object with his ID matching the request.params.id 
-    if (!gamesByID) {
-        return response.status(404).json({ message: 'Game not found' });
-    }
-    // If the game is not found, I return a 404 status and a message
-    return response.status(200).json(gamesByID);
-    //
+    return response.send('Welcome to the Games API');
 });
 
-app.post('/games', (request, response) => {
-    const {title, genre } = request.body;
-    if (!title , !genre) {
-        return response.status(400).json({ message: 'Title and genre are required' });
-    }
-    const newGames = {
-        id: games.length + 1,
-        title,
-        genre
-    };
-    games.push(newGames);
-    return response.status(201).json(newGames);
-});
-
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
     }
 );
